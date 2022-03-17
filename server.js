4dfcc22d24485e76924d4f914180786b9004b3f9
najibjodiansyah <najibjodiansyah11@gmail.com>
2022-03-17 13:05:54 +0700

    routing req

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const http = require('http')
+const http = require("http");
 
 /**
  * Logika untuk menangani dan menanggapi request dituliskan pada fungsi ini
@@ -7,44 +7,78 @@ const http = require('http')
  * @param response: objek yang digunakan untuk menanggapi permintaan
  */
 const requestListener = (request, response) => {
-	response.setHeader('Content-Type', 'text/html');
- 
-    	response.statusCode = 200;
+	response.setHeader("Content-Type", "text/html");
 
-	const {method} = request;
+	response.statusCode = 200;
 
-	if(method === 'GET') {
-        response.end('<h1> Menggunakan method GET</h1>');
-    }
+	const { method, url } = request;
 
-    if(method === 'POST') {
-        let body = [];
-    
-  request.on('data', (chunk) => {
-      body.push(chunk);
-  });
- 
-  request.on('end', () => {
-      body = Buffer.concat(body).toString();
-      const { name } = JSON.parse(body);
-      response.end(`<h1>Hai, ${name}!</h1>`);
-  });
-    }
+	if (url === "/") {
+		if (method === "GET") {
+			response.end("<h1>Ini adalah homepage</h1>");
+		} else {
+			response.end(
+				`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
+			);
+		}
+	} else if (url === "/about") {
+		if (method === "GET") {
+			response.end("<h1>Halo! Ini adalah halaman about</h1>");
+		} else if (method === "POST") {
+			let body = [];
 
-    if(method === 'PUT') {
-        response.end('<h1>Menggunakan method PUT</h1>');
-    }
+			request.on("data", (chunk) => {
+				body.push(chunk);
+			});
 
-    if(method === 'DELETE') {
-        response.end('<h1>menggunakan method DELETE</h1>');
-    }
+			request.on("end", () => {
+				body = Buffer.concat(body).toString();
+				const { name } = JSON.parse(body);
+				response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+			});
+		} else {
+			response.end(
+				`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`
+			);
+		}
+	} else {
+		response.end("<h1>Halaman Tidak Ditemukan</h1>");
+	}
+
+	// const { method } = request;
+
+	// if (method === "GET") {
+	// 	response.end("<h1> Menggunakan method GET</h1>");
+	// }
+
+	// if (method === "POST") {
+	// 	let body = [];
+
+	// 	request.on("data", (chunk) => {
+	// 		body.push(chunk);
+	// 	});
+
+	// 	request.on("end", () => {
+	// 		body = Buffer.concat(body).toString();
+	// 		const { name } = JSON.parse(body);
+	// 		response.end(`<h1>Hai, ${name}!</h1>`);
+	// 	});
+	// }
+
+	// if (method === "PUT") {
+	// 	response.end("<h1>Menggunakan method PUT</h1>");
+	// }
+
+	// if (method === "DELETE") {
+	// 	response.end("<h1>menggunakan method DELETE</h1>");
+	// }
 };
 
 const server = http.createServer(requestListener);
 
 const port = 5000;
-const host = 'localhost';
+const host = "localhost";
 
 server.listen(port, host, () => {
-    console.log(`Server berjalan pada http://${host}:${port}`);
+	console.log(`Server berjalan pada http://${host}:${port}`);
 });
